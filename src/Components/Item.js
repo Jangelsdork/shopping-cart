a56224e9f3c801e724024d574572753915ac0aa9
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-03-25 20:37:59 +0100

    add map to render shop items

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,7 +1,14 @@
-export default function Item(){
+export default function Item( items ){
 
     
     return(
-    <div>fads</div>
+        <div className="item">
+        <div
+          className="image"
+          style={{ backgroundColor: items.colorId }}
+        ></div>
+        <div className="title">{items.itemName}</div>
+        <div className="price">${items.itemPrice}</div>
+      </div>
     )
 }
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,3 +1,5 @@
+import Item from "./Item";
+
 export default function Shop({ items }) {
   return (
     <div>
@@ -7,30 +9,16 @@ export default function Shop({ items }) {
         <a href="/cart">Cart</a>
       </div>
       <div className="shopContainer">
-        <div className="item">
-          <div
-            className="image"
-            style={{ backgroundColor: items[0].colorId }}
-          ></div>
-          <div className="title">{items[0].itemName}</div>
-          <div className="price">${items[0].itemPrice}</div>
-        </div>
-        <div className="item">
-          <div
-            className="image"
-            style={{ backgroundColor: items[1].colorId }}
-          ></div>
-          <div className="title">{items[1].itemName}</div>
-          <div className="price">${items[1].itemPrice}</div>
-        </div>
-        <div className="item">
-          <div
-            className="image"
-            style={{ backgroundColor: items[2].colorId }}
-          ></div>
-          <div className="title">{items[2].itemName}</div>
-          <div className="price">${items[2].itemPrice}</div>
-        </div>
+        {items.map((anItem) => {
+            return(
+                <Item 
+                    itemName={anItem.itemName}
+                    itemPrice={anItem.itemPrice}
+                    colorId={anItem.colorId}
+                    />
+            )
+        })
+        }
       </div>
     </div>
   );
